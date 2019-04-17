cb6d7482b04b308af27e1a7c0f7bc038224dffe0
pksX01 <pksX01@gmail.com>
2019-04-18 01:56:28 +0530

    Version 1.1

--- app/src/main/java/com/ojasxlabs/cricketlogics/MainActivity.java
+++ app/src/main/java/com/ojasxlabs/cricketlogics/MainActivity.java
@@ -4,6 +4,7 @@ package com.ojasxlabs.cricketlogics;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,26 +14,30 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    long no_of_matches;
+
     int i=1;
     private String team1,team2,winner,date;
     private ListView mListView;
-    private ArrayList<String> data= new ArrayList<>();
-
+    private ArrayList<String> data;
+    Match match;
     private static DatabaseReference db_ref;
     private static FirebaseDatabase db;
 
@@ -42,20 +47,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        match = new Match();
+        db = FirebaseDatabase.getInstance();
+        db_ref=db.getReference();
+       if(db==null) {
+
+            db.setPersistenceEnabled(true);
+       }
+
+
+
 
         MobileAds.initialize(this, "ca-app-pub-4327627819814341~7447187245");
 
+
+
         mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("9762F7D37EE798D43AF75C9C8573B8ED").build();
         mAdView.loadAd(adRequest);
 
-       if(db==null) {
-            db = FirebaseDatabase.getInstance();
-            db.setPersistenceEnabled(true);
-        }
-        db_ref=db.getReference();
+
+
         mListView = (ListView) findViewById(R.id.list);
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data){
+        data = new ArrayList<>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data)
+        {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent){
@@ -76,46 +92,41 @@ public class MainActivity extends AppCompatActivity {
 
 
         };
+
+db_ref.addChildEventListener(new ChildEventListener() {
+    @Override
+    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+        //for(DataSnapshot ds : dataSnapshot.getChildren()){
+
+            match = dataSnapshot.getValue(Match.class);
+            data.add("Match "+ (i++) + " : " + match.getDate());
+            data.add(match.getTeam1() + " vs " + match.getTeam2());
+            data.add("Winner : " + match.getWinner());
+
+       // }
         mListView.setAdapter(arrayAdapter);
+    }
 
-        db_ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                no_of_matches = dataSnapshot.getChildrenCount();
-
-                //Toast.makeText(getApplicationContext(),"no of matches = "+ no_of_matches,Toast.LENGTH_LONG).show();
-                for(DataSnapshot ds : dataSnapshot.getChildren()){
-                    team1=ds.child("Team 1").getValue(String.class);
-                    team2=ds.child("Team 2").getValue(String.class);
-                    winner=ds.child("Winner").getValue(String.class);
-                    date=ds.child("Date").getValue(String.class);
-
-                    data.add("Match "+ (i++) + " : "+date);
-                    //text.setTextColor(Color.BLACK);
-                    data.add(team1+" vs "+team2);
-                    //text.setTextColor(Color.GREEN);
-                    data.add("Winner : " + winner);
-                    //data.add(team1+" "+team2+" "+winner+" "+date);
-                    //data.add(team2);
-
-                    //Toast.makeText(getApplicationContext(),team1+" "+team2+" "+winner+" "+date+" ",Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
+    @Override
+    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
     }
 
-   /* @Override
-    public void onStart(){
-        super.onStart();
+    @Override
+    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
 
+    }
 
-    }*/
+    @Override
+    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+    }
+});
+
+    }
 }
