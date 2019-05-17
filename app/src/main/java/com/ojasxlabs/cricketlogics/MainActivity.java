62a3790a26e964adcd39b04742e7fb48c92dd8b2
pksX01 <pksX01@gmail.com>
2019-05-17 17:52:01 +0530

    World Cup Schedule

--- app/src/main/java/com/ojasxlabs/cricketlogics/MainActivity.java
+++ app/src/main/java/com/ojasxlabs/cricketlogics/MainActivity.java
@@ -1,80 +1,71 @@
 package com.ojasxlabs.cricketlogics;
 
-
-import android.graphics.Color;
-import android.support.annotation.NonNull;
-
-import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-
-import com.google.android.gms.ads.AdListener;
+import android.support.v7.widget.Toolbar;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-    int i=1;
+    /*int i=1;
     private String team1,team2,winner,date;
     private ListView mListView;
     private ArrayList<String> data;
     Match match;
     private static DatabaseReference db_ref;
-    private static FirebaseDatabase db;
+    private static FirebaseDatabase db;*/
 
     private AdView mAdView;
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        match = new Match();
-        db = FirebaseDatabase.getInstance();
-        db_ref=db.getReference();
-       if(db==null) {
-
-            db.setPersistenceEnabled(true);
-       }
-
-
-
-
-        MobileAds.initialize(this, "ca-app-pub-4327627819814341~7447187245");
-
-
-
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713 ");
         mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().addTestDevice("9762F7D37EE798D43AF75C9C8573B8ED").build();
+        AdRequest adRequest = new AdRequest.Builder()/*.addTestDevice("9762F7D37EE798D43AF75C9C8573B8ED")*/.build();
         mAdView.loadAd(adRequest);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mListView = (ListView) findViewById(R.id.list);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        /* match = new Match();
+                db = FirebaseDatabase.getInstance();
+                db_ref=db.getReference();
+               if(db==null) {
+
+                    db.setPersistenceEnabled(true);
+               }*/
+       /* mListView = (ListView) findViewById(R.id.list);
         data = new ArrayList<>();
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data)
         {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent){
+
                 // Get the Item from ListView
                 View view = super.getView(position, convertView, parent);
 
@@ -88,45 +79,82 @@ public class MainActivity extends AppCompatActivity {
                     tv.setTextColor(Color.BLUE);
                 // Generate ListView Item using TextView
                 return view;
+
             }
 
 
         };
 
-db_ref.addChildEventListener(new ChildEventListener() {
-    @Override
-    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-        //for(DataSnapshot ds : dataSnapshot.getChildren()){
+            db_ref.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    //for(DataSnapshot ds : dataSnapshot.getChildren()){
 
-            match = dataSnapshot.getValue(Match.class);
-            data.add("Match "+ (i++) + " : " + match.getDate());
-            data.add(match.getTeam1() + " vs " + match.getTeam2());
-            data.add("Winner : " + match.getWinner());
+                        match = dataSnapshot.getValue(Match.class);
+                        data.add("Match "+ (i++) + " : " + match.getDate());
+                        data.add(match.getTeam1() + " vs " + match.getTeam2());
+                        data.add("Winner : " + match.getWinner());
 
-       // }
-        mListView.setAdapter(arrayAdapter);
+                   // }
+                    mListView.setAdapter(arrayAdapter);
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });*/
+
     }
 
-    @Override
-    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new WorldCupScheduleFragment(), "World Cup Schedule");
+        adapter.addFragment(new MatchPrediction(), "Match Prediction");
+        viewPager.setAdapter(adapter);
     }
 
-    @Override
-    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-    }
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
 
-    @Override
-    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
 
-    }
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
 
-    @Override
-    public void onCancelled(@NonNull DatabaseError databaseError) {
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
 
-    }
-});
-
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
--- app/src/main/java/com/ojasxlabs/cricketlogics/WelcomeScreen.java
+++ app/src/main/java/com/ojasxlabs/cricketlogics/WelcomeScreen.java
@@ -25,7 +25,7 @@ public class WelcomeScreen extends AppCompatActivity {
         textView=(TextView)findViewById(R.id.textView);
         textView.setText("");
 
-        final long period = 100;
+        final long period = 60;
         timer=new Timer();
         timer.schedule(new TimerTask() {
             @Override
